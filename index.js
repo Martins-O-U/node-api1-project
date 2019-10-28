0a3ee7453084d712621f11f3a3c61ba66afd4b1d
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-28 15:20:18 +0100

    Added a get users and get Users by Id

--- index.js
+++ index.js
@@ -2,14 +2,41 @@
 
 const express = require('express')
 const cors = require('cors')
-
 const db = require('./data/db')
+
 const server = express()
 
 server.use(cors())
 server.use(express.json())
 
+server.get('/api/users/:id', getUserById)
+server.get('/api/users', getUsers)
 server.get('*', handleDefault)
+
+function getUserById(req, res){
+    
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(data =>{
+            console.log(data)
+            res.status(200).json(data)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+}
+
+function getUsers (req, res){
+    db.find()
+        .then(data=>{
+            console.log(data)
+            res.json(data)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+}
 
 function handleDefault (req, res){
     res.json('Test Succeeded!')

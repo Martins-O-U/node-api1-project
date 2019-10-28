2273f38c5b8f0abf590d39910e8b0eca3d647fd7
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-28 14:12:15 +0100

    All 8 steps implimented - Test successful

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+
+const express = require('express')
+const cors = require('cors')
+
+const db = require('./data/db')
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.get('*', handleDefault)
+
+function handleDefault (req, res){
+    res.json('Test Succeeded!')
+}
+
+server.listen(process.env.port || 3000, () => {
+    console.log('listening on ' + (process.env.port || 3000))
+})
